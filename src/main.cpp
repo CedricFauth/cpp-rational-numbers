2b4c310eb1c737b386a2666a90d3fc27d462792c
Cedric Fauth <cedricfauth@yahoo.de>
2021-01-28 16:11:21 +0100

    first tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 
 int main(int argc, char const *argv[])
 {
-	QNumber q1 (6,8);
-	QNumber q2 (0.3);
+	QNumber q0;
+	QNumber q1 = 2;
+	QNumber q2 (6, 8);
+	QNumber q3 = 0.2;
+	QNumber *q4 = new QNumber;
+	delete q4;
 	std::cout << q1 << '\n' << q2 << std::endl;
 	return 0;
 }
